1730e248d21e9a1a9cb6a7ba39039e940d911595
Dilawar Singh <dilawar@subcom.tech>
2023-12-17 11:21:37 +0530

    doc: Adds example

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,15 @@
 //! Extended cron shorthands that support sampling from given interval. In addition to standard
 //! expression supported by excellent crate cron, we support following type of expressions.
 //!
-//! - `@daily{H=9-17}` means run once between 9am and 5pm chosen randomly.  
-//! - `@daily{H=9-12,H=15-20}` means run once between 9am and 12pm or between 3pm and 8pm.
+//! - `@daily{h=9-17}` means run once between 9am and 5pm chosen randomly.  
+//! - `@daily{h=9-12,h=15-20}` means run once between 9am and 12pm or between 3pm and 8pm.
 //!
 //! Similarly one can pass daily contraints to @weekly.
 //!
-//! - `@weekly{D=1-5}` mean  run once per week between day 1 and day 5.  
-//! - `@weekly{D=1-5,H=9-12}` run once per week between day 1 and day 5 and between 9am
+//! - `@weekly{d=1-5}` mean  run once per week between day 1 and day 5.  
+//! - `@weekly{d=1-5,h=9-12}` run once per week between day 1 and day 5 and between 9am
 //!    and 12pm.  
-//! - `@weekly{H=9-12}` run once per week at any day chosen randomly and between 9am
+//! - `@weekly{h=9-12}` run once per week at any day chosen randomly and between 9am
 //!    and 12pm.
 
 use std::collections::HashMap;
